4291da3745a311d52aee365476583fc90b1b99ec
ecureuill <682349-ecureuill@users.noreply.gitlab.com>
2024-02-28 21:38:43 -0300

    🎉

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Portfolio
+
+This repository contains the source code for my portfolio page. 
+### Design and Technologies
+
+* **Design:** The design of the website was created using [Penpot](https://penpot.app), an open-source alternative to Figma.
+* **Development:** The website is built using HTML5, CSS3, and JavaScript.
+* **Accessibility:** The website has been developed with accessibility in mind, following best practices to ensure it is usable for everyone.
+* **Tools**
+  * [Fluid Typography](https://fluidtypography.com/) to create a dynamic and responsive reading experience across various screen sizes.
+  * [Contrast-Finder](app.contrast-finder.org): Ensures optimal color contrast for web accessibility by suggesting suitable contrasts for your chosen colors and providing WCAG compliance details.
+
+### Project Structure
+
+The project is organized into the following folders:
+
+* **assets:** Contains images and other static assets used in the website.
+* **scripts:** Contains any JavaScript code used on the website.
+* **styles:** Contains the CSS styles for the website.
+
+**Image Sources:**
+
+It's important to note that the following images were generated using Gemini:
+
+* Hero image (`hero.png`)
+* Laptop image (`laptop.png`)
+* LinkedIn icon (`linkedin.png`)
+* Screens image (`2-screens.png` and `3-screens.png`)
---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const carousel = document.getElementById('projects-frontend-carousel');
+  const nextBtn = carousel.querySelector('.--carousel-controll-next');
+  const prevBtn = carousel.querySelector('.--carousel-controll-previous');
+  const carouselItems = carousel.querySelectorAll('.--carousel-item');
+  let currentIndex = 0;
+
+  nextBtn.addEventListener('click', () => changeActiveSlide('next'));
+  prevBtn.addEventListener('click', () => changeActiveSlide('prev'));
+
+  function changeActiveSlide(direction) {
+    const numSlides = carouselItems.length;
+    const loopedIndex = (currentIndex + (direction === 'next' ? 1 : -1) + numSlides) % numSlides;
+
+    carouselItems[currentIndex].classList.remove('active');
+    carouselItems[loopedIndex].classList.add('active');
+
+    currentIndex = loopedIndex;
+  }
+});
+
+function flip(element) {
+  const cardInner = element.querySelector(".flip-card-inner");
+
+  cardInner.classList.toggle("flip");
+
+  const frontFace = cardInner.querySelector(".flip-card-front");
+  const backFace = cardInner.querySelector(".flip-card-back");
+
+  const isFlipped = cardInner.classList.contains("flip");
+  frontFace.setAttribute("aria-hidden", isFlipped ? "false" : "true");
+  backFace.setAttribute("aria-hidden", isFlipped ? "true" : "false");
+
+  const adjustLinks = (face) => {
+    const links = face.querySelectorAll("a");
+    links.forEach(link => {
+      if (isFlipped) {
+        link.removeAttribute("tabindex");
+        link.removeAttribute("aria-disabled");
+      } else {
+        link.setAttribute("tabindex", "-1");
+        link.setAttribute("aria-disabled", "true");
+      }
+    });
+  };
+
+  adjustLinks(frontFace);
+  adjustLinks(backFace);
+}
